dad9c757e2a5d154f1528f4509961834a34208d8
solakqqq <solawit2542@gmail.com>
2026-06-03 22:08:16 +0700

    edit detail

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,13 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('navigation', () => {
-  test.beforeEach(async ({ page }) => {
-    // Go to the starting url before each test.
-    await page.goto('https://www.w3schools.com');
-  });
-
   test('main navigation', async ({ page }) => {
-    // Assertions use the expect API.
-    await expect(page).toHaveURL('https://www.w3schools.com');
+   await page.goto('https://www.w3schools.com');
   });
-});
