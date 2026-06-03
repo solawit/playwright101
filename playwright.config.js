793faad99780f91b095a024bc8acf2db81034333
solakqqq <solawit2542@gmail.com>
2026-06-03 22:22:42 +0700

    add config

--- playwright.config.js
+++ playwright.config.js
@@ -14,21 +14,16 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-
   fullyParallel: true,
-
   forbidOnly: !!process.env.CI,
-
   retries: process.env.CI ? 2 : 0,
-
   workers: process.env.CI ? 1 : undefined,
-
   reporter: 'html',
 
   use: {
     
     trace: 'on-first-retry',
-    headless: false,
+    headless: true,
     screenshot: "on",
     video: { mode: "on", size: { width: 768, height: 1000 } },
   },
